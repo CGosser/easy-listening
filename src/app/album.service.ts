d28f4fc0a0edbd119c614770429252624132adf9
Chris Gosser and Krystal Foster <krystal@>
2018-10-23 10:46:42 -0700

    stuff n things

---
+++ src/app/album.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+  this.albums = database.list('albums');
+  }
+  getAlbums() {
+    return this.albums;
+  }
+
+  getAlbumById(albumId: number){
+     return this.database.object('albums/' + albumId);
+  }
+
+}
