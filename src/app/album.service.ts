83288f60b22a3b22b75a08ae0c4f5add56e4808a
Chris Gosser and Krystal Foster <chris@>
2018-10-23 16:17:46 -0700

    delete

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -20,4 +20,14 @@ export class AlbumService {
     this.albums.push(newAlbum);
   }
 
+  updateAlbum(localUpdatedAlbum){
+    var albumEntryInFirebase = this.getAlbumById(localUpdatedAlbum.$key);
+    albumEntryInFirebase.update({title: localUpdatedAlbum.title,
+                                artist: localUpdatedAlbum.artist,
+                                description: localUpdatedAlbum.description});
+}
+deleteAlbum(localAlbumToDelete){
+    var albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 }
--- src/app/edit-album/edit-album.component.ts
+++ src/app/edit-album/edit-album.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 @Component({
   selector: 'app-edit-album',
   templateUrl: './edit-album.component.html',
-  styleUrls: ['./edit-album.component.css']
+  styleUrls: ['./edit-album.component.css'],
+   providers: [AlbumService]
 })
 export class EditAlbumComponent implements OnInit {
-
-  constructor() { }
+  @Input() selectedAlbum;
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit() {
   }
+  beginUpdatingAlbum(albumToUpdate){
+   this.albumService.updateAlbum(albumToUpdate);
+ }
 
+ beginDeletingAlbum(albumToDelete){
+    if(confirm("Are you sure you want to delete this item from the inventory?")){
+      this.albumService.deleteAlbum(albumToDelete);
+    }
+  } 
 }
